5c373e26b01e8dd30cf74cdbb772d1d62297ce53
Jack Hsu <jack.hsu@gmail.com>
2019-03-06 09:04:07 -0500

    initial commit

---
+++ apps/my-app/jest.config.js
@@ -0,0 +1,13 @@
+module.exports = {
+  name: "my-app",
+  preset: "../../jest.config.js",
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
+  coverageDirectory: "../../coverage/apps/my-app/",
+  snapshotSerializers: [
+    "jest-preset-angular/AngularSnapshotSerializer.js",
+    "jest-preset-angular/HTMLCommentSerializer.js"
+  ]
+};
