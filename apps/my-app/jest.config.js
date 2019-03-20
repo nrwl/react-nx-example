c374f511c5cb4078bb7c9fb333c194a9daf8f674
Jack Hsu <jack.hsu@gmail.com>
2019-03-20 09:21:11 -0400

    initial commit

---
+++ apps/my-app/jest.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: "my-app",
+  preset: "../../jest.config.js",
+  transform: {
+    "^.+\\.[tj]sx?$": "ts-jest"
+  },
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "html"],
+  coverageDirectory: "../../coverage/apps/my-app/"
+};
